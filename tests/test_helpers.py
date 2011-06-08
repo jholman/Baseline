4e6b07d34e76bfeec30d371e6417ed9934729c70
Jeremy Holman <jeremy.holman@gmail.com>
2011-06-08 02:19:49 -0700

    misc test cleanup, including space-after-comma whitespace fix

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -7,19 +7,19 @@ class TestDeque():
         d = deque()
         d.append(1)
         d.append('b')
-        d.append([[],[],[]])
-        assert list(d) == [1, 'b', [[],[],[]]]
+        d.append([[], [], []])
+        assert list(d) == [1, 'b', [[], [], []]]
         d.append('', 0)
         d.append(4)
         d.append('negative one', 0)
-        assert list(d) == ['negative one', '', 1, 'b', [[],[],[]], 4]
+        assert list(d) == ['negative one', '', 1, 'b', [[], [], []], 4]
 
     def test_init_and_pop(self):
-        d = deque(1,2,3,4)
+        d = deque(1, 2, 3, 4)
         d.append(5)
-        d.append(0,0)
+        d.append(0, 0)
         d.append(6)
-        d.append(-1,0)
+        d.append(-1, 0)
         x = d.pop()
         x = d.pop()
         x = d.pop()
@@ -33,9 +33,9 @@ class TestDeque():
     def test_len_and_extend(self):
         d = deque()
         assert len(d) == 0
-        d.append(1,0)
+        d.append(1, 0)
         assert len(d) == 1
-        d.extend(['a','b','c'])
+        d.extend(['a', 'b', 'c'])
         assert len(d) == 4
         x = d.pop(0)
         x = d.pop(0)
@@ -74,11 +74,9 @@ class TestDStack():
         ds.split(1, 0)
         assert map(list, ds) == [[2, 3], [1]]
         ds.append(4)
-        ds.append(5,0)
+        ds.append(5, 0)
         assert map(list, ds) == [[2, 3], [5, 1, 4]]
-        print ds
         ds.merge(0)
-        print ds
         assert map(list, ds) == [[5, 1, 4, 2, 3]]
 
     def test_split_merge_degenerate(self):
@@ -89,20 +87,10 @@ class TestDStack():
         assert map(list, ds) == [[1, 2, 3]]
         ds.merge()
         assert map(list, ds) == [[1, 2, 3]]
-
-    def not_test_split_merge(self):
+        ds.split(4)
+        assert map(list, ds) == [[], [1, 2, 3]]
         ds.split(2)
-        assert map(list, ds) == [[1], [], [2, 3]]
-        ds.split(5)
-        assert map(list, ds) == [[1], [], [], [2, 3]]
-        ds.extend([4,5])
-        ds.split(2,0)
-        assert map(list, ds) == [[1], [], [], [4, 5], [2, 3]]
-        ds.merge()
-        ds.merge()
-        assert map(list, ds) == [[1], [], [4, 5, 2, 3]]
-
-    ### TODO: test "pop" and also I have no working "reverse" split/merge
+        assert map(list, ds) == [[], [1], [2, 3]]
 
     def test_pop(self):
         ds = dstack([1, 2, 3, 4, 5])
